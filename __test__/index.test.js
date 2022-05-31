8d2fea32a44bbf4998232a0f18367d1e09c1ac93
Suryapratap Singh <suryprtaps@gmail.com>
2022-05-31 20:51:50 +0530

    add file authorship and purpose of creating

--- __test__/index.test.js
+++ __test__/index.test.js
@@ -1,5 +1,5 @@
 /**
  * @author Suryapratap Singh Suryavanshi
  * @date 31/05/2022
- * @summary This file contains main hierarchy of the module.
+ * @summary This file contains all the test related to module
  */
---
+++ index.d.ts
@@ -0,0 +1,5 @@
+/**
+ * @author Suryapratap Singh Suryavanshi
+ * @date 31/05/2022
+ * @summary This file contains all the typescript declaration related to javascript methods.
+ */
